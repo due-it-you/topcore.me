7ae86b4c3f667b23fa12ac0fca582a461120d147
dueityou <dueityou@gmail.com>
2025-06-14 20:38:38 +0900

    [fix] wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Layout from './components/Layout';
+import Layout from './components/layout/Layout';
 import Header from './components/Header';
 import AlbumSearchCard from './components/ui/album_search/AlbumSearchCard';
 import CreateGridBody from './components/layout/CreateGridBody';
