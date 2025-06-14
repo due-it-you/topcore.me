478e53729e7e407aa147401ee63ce87295afcb3b
dueityou <dueityou@gmail.com>
2025-06-14 14:37:31 +0900

    [add] Headerコンポーネントの表示

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import Layout from './components/layout/layout';
+import Header from './components/common/header';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Layout />
+      <Layout>
+        <Header />
+      </Layout>
     </>
   );
 }
