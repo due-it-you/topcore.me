0ba04a55845a1c6a53a9abbb2b39ef392b66dea1
dueityou <dueityou@gmail.com>
2025-06-14 20:05:32 +0900

    [add] グリッドの各マスの共通となるAlbumコンポーネントの新規作成 #3

---
+++ frontend/src/components/ui/drop_album_grid/Album.jsx
@@ -0,0 +1,11 @@
+export default function Album() {
+  return (
+    <div className="aspect-square rounded-xl bg-[#0E1012]">
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
+        alt="set_album"
+        className="rounded-sm"
+      />
+    </div>
+  );
+}
