ce98e45c4175b46650b86b42a1d20f430becffb2
dueityou <dueityou@gmail.com>
2025-06-14 16:47:58 +0900

    [add] 検索のためのフィールドとボタンを扱うUIコンポーネントの新規作成 #1

---
+++ frontend/src/components/ui/album_search/SearchBar.jsx
@@ -0,0 +1,20 @@
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+export default function SearchBar() {
+  return (
+    <div className="flex items-center justify-center">
+      <Input
+        className="w-3/4 bg-[#252A30] text-[#C4C6C9]"
+        placeholder="検索したいアルバムを入力してください"
+      />
+      <Button
+        variant="secondary"
+        size="icon"
+        className="mx-2 size-10 bg-[#252A30] hover:bg-[#1E2328]"
+      >
+        🔍
+      </Button>
+    </div>
+  );
+}
