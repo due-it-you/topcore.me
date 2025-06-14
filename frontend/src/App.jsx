70c90e6530e8d6430c1bf188055f6966f11609ff
dueityou <dueityou@gmail.com>
2025-06-14 15:57:49 +0900

    [change] AlbumSearchCardコンポーネントファイルをui/album_search配下に移動

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Layout from './components/layout/Layout';
 import Header from './components/common/Header';
-import AlbumSearchCard from './components/ui/AlbumSearchCard';
+import AlbumSearchCard from './components/ui/album_search/AlbumSearchCard';
 import CreateGridBody from './components/layout/CreateGridBody';
 
 function App() {
