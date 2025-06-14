678dfb403647a5a2fede03df35991fab9fe54eff
dueityou <dueityou@gmail.com>
2025-06-14 17:12:44 +0900

    [update]　子要素の高さを指定するために親要素の明示的な高さを指定するように更新 #1

--- frontend/src/components/ui/album_search/AlbumSearchCard.jsx
+++ frontend/src/components/ui/album_search/AlbumSearchCard.jsx
@@ -3,7 +3,7 @@ export default function AlbumSearchCard({ children }) {
     <>
       <div className="flex h-screen w-2/5 items-center">
         <div className="mx-4 h-4/5 w-full rounded-2xl bg-[#151A1E] shadow-2xl">
-          <div className="p-12">{children}</div>
+          <div className="h-full p-12">{children}</div>
         </div>
       </div>
     </>
