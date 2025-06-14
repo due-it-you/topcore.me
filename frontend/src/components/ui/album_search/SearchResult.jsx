23f9f8d0e93c88aa683e40266b1f203d0afbee8b
dueityou <dueityou@gmail.com>
2025-06-14 17:43:23 +0900

    [update] SearchResultコンポーネントの背景色を削除

--- frontend/src/components/ui/album_search/SearchResult.jsx
+++ frontend/src/components/ui/album_search/SearchResult.jsx
@@ -1,6 +1,6 @@
 export default function SearchResult({ children }) {
   return (
-    <div className="h-7/8 bg-gray-400">
+    <div className="h-7/8">
       <div className="grid grid-cols-4 gap-1">{children}</div>
     </div>
   );
