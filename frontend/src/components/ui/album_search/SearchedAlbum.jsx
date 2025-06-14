a76f4b7cb0fd57bf19df7f1f00baa987ef0a49c8
dueityou <dueityou@gmail.com>
2025-06-14 17:42:25 +0900

    [add] 検索結果の各アルバムに共通するSearchedAlbumコンポーネントの新規追加

---
+++ frontend/src/components/ui/album_search/SearchedAlbum.jsx
@@ -0,0 +1,13 @@
+export default function SearchedAlbum() {
+  return (
+    <div>
+      <div className="aspect-square rounded-xl bg-white">
+        <img
+          src="https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
+          alt="album_image"
+          className="rounded-xl"
+        />
+      </div>
+    </div>
+  );
+}
