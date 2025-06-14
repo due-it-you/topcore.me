6f85be8d19e58d1df2b8873c30c6f41cb6e5dfc1
dueityou <dueityou@gmail.com>
2025-06-14 16:31:53 +0900

    [add] AlbumSearchCardの子コンポーネントに対してpaddingを追加 #1

--- frontend/src/components/ui/album_search/AlbumSearchCard.jsx
+++ frontend/src/components/ui/album_search/AlbumSearchCard.jsx
@@ -1,9 +1,9 @@
-export default function AlbumSearchCard({children}) {
+export default function AlbumSearchCard({ children }) {
   return (
     <>
       <div className="flex h-screen w-2/5 items-center">
         <div className="mx-4 h-4/5 w-full rounded-2xl bg-[#151A1E] shadow-2xl">
-          <div>{children}</div>
+          <div className="p-12">{children}</div>
         </div>
       </div>
     </>
