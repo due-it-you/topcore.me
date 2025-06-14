ba69aa9f877ab495faf34fb060fea1ed71034fea
dueityou <dueityou@gmail.com>
2025-06-14 20:39:39 +0900

    [fix] ビルド時のimport失敗のエラーを修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Layout from './components/layout/Layout';
-import Header from './components/Header';
+import Header from './components/common/Header';
 import AlbumSearchCard from './components/ui/album_search/AlbumSearchCard';
 import CreateGridBody from './components/layout/CreateGridBody';
 import SearchBar from './components/ui/album_search/SearchBar';
