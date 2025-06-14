3dfdc3fe4b07f13ee8c0252260c0af4e07f5dd6f
dueityou <dueityou@gmail.com>
2025-06-14 14:36:34 +0900

    [add] ヘッダーのコンポーネントの新規作成

---
+++ frontend/src/components/common/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <div className="h-16 w-full bg-[#131313]">
+        <div className="px-4">
+          <img
+            src="./../../../public/top_core_logo_trimmed.png"
+            alt="top_core_me_logo"
+            className="h-16"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
