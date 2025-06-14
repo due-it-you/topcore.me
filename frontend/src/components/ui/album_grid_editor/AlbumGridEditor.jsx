f37a19e25fbd4ba7fabceae9485b6c934779309e
dueityou <dueityou@gmail.com>
2025-06-15 00:33:58 +0900

    [refactor] AlbumGridEditorのグリッド箇所をDropAlbumGridコンポーネントとして切り出しました

--- frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
+++ frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
@@ -1,6 +1,7 @@
 import ColorPalette from '../color_palette/ColorPalette';
 import Album from './Album';
 import { Button } from '@/components/ui/button';
+import DropAlbumGrid from './DropAlbumGrid';
 
 
 export default function AlbumGridEditor() {
@@ -8,17 +9,7 @@ export default function AlbumGridEditor() {
     <div className="flex h-screen w-3/5 items-center justify-center">
       <div className="mx-4 flex aspect-square w-3/4">
         <div className="h-full p-12">
-          <div className="grid grid-cols-3 grid-rows-3 gap-2">
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-            <Album />
-          </div>
+          <DropAlbumGrid />
           <div className="mt-4 text-right">
             <Button className="bg-[#20C997] font-semibold text-white hover:bg-[#1CB588]">
               リンクを生成
