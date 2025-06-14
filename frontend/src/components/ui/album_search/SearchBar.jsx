704cb7b80389e85f79cd85c0e3294c158cf11a56
dueityou <dueityou@gmail.com>
2025-06-14 17:14:12 +0900

    [update] 検索フィールドと検索結果を少し離して表示したいため、検索フィールドの下に余白を追加

--- frontend/src/components/ui/album_search/SearchBar.jsx
+++ frontend/src/components/ui/album_search/SearchBar.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 
 export default function SearchBar() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="mb-4 flex items-center justify-center">
       <Input
         className="w-3/4 bg-[#252A30] text-[#C4C6C9]"
         placeholder="検索したいアルバムを入力してください"
