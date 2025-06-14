4d60bb57ceeae8676f4a06b9e25f9c805f297642
dueityou <dueityou@gmail.com>
2025-06-14 21:38:00 +0900

    [refactor] SearchResultも中身はSearchedAlbumコンポーネントで固定されているので、SearchedAlbumをSearchResultコンポーネントの中に移動 #7

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import AlbumSearchCard from './components/ui/album_search/AlbumSearchCard';
 import CreateGridBody from './components/layout/CreateGridBody';
 import SearchBar from './components/ui/album_search/SearchBar';
 import SearchResult from './components/ui/album_search/SearchResult';
-import SearchedAlbum from './components/ui/album_search/SearchedAlbum';
 import DropAlbumGrid from './components/ui/drop_album_grid/DropAlbumGrid';
 
 function App() {
@@ -16,31 +15,7 @@ function App() {
         <CreateGridBody>
           <AlbumSearchCard>
             <SearchBar />
-            <SearchResult>
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-              <SearchedAlbum />
-            </SearchResult>
+            <SearchResult />
           </AlbumSearchCard>
           <DropAlbumGrid />
         </CreateGridBody>
