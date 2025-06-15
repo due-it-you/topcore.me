f40213a7eb757a81b088ce8b8149c60bc1dd6920
dueityou <dueityou@gmail.com>
2025-06-15 21:01:22 +0900

    [add] アルバム検索フィールドの入力値の状態を保持するstateを追加 #2

--- frontend/src/components/ui/album_search/AlbumSearchCard.jsx
+++ frontend/src/components/ui/album_search/AlbumSearchCard.jsx
@@ -5,6 +5,7 @@ import axios from '../../../../api/lib/apiClient';
 
 export default function AlbumSearchCard() {
   const [albums, setAlbums] = useState(null);
+  const [searchAlbumInput, setSearchAlbumInput] = useState(null);
   async function onSearchClick() {
     const res = await axios.get('/albums/search');
     const albums = res.data.searchedAlbums;
@@ -15,7 +16,7 @@ export default function AlbumSearchCard() {
     <div className="flex h-screen w-2/5 items-center">
       <div className="mx-4 h-4/5 w-full rounded-2xl bg-[#151A1E] shadow-2xl">
         <div className="h-full p-12">
-          <SearchBar onSearchClick={onSearchClick} />
+          <SearchBar onSearchClick={onSearchClick} setSearchAlbumInput={setSearchAlbumInput} />
           <SearchResult albums={albums} />
         </div>
       </div>
--- frontend/src/components/ui/album_search/SearchBar.jsx
+++ frontend/src/components/ui/album_search/SearchBar.jsx
@@ -1,12 +1,13 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
-export default function SearchBar({ onSearchClick }) {
+export default function SearchBar({ onSearchClick, setSearchAlbumInput }) {
   return (
     <div className="mb-4 flex items-center justify-center">
       <Input
         className="w-3/4 bg-[#252A30] text-[#C4C6C9]"
         placeholder="検索したいアルバムを入力してください"
+        onChange={e => setSearchAlbumInput(e.target.value)}
       />
       <Button
         onClick={onSearchClick}
