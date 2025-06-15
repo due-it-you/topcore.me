64b3dc26fcc20b176e70b5ed39f360ed84d94e01
dueityou <dueityou@gmail.com>
2025-06-16 03:03:31 +0900

    [update] アルバム検索カードに影のCSSを追加

--- frontend/src/components/ui/album_search/AlbumSearchCard.jsx
+++ frontend/src/components/ui/album_search/AlbumSearchCard.jsx
@@ -14,7 +14,7 @@ export default function AlbumSearchCard() {
 
   return (
     <div className="flex h-screen w-2/5 items-center">
-      <div className="mx-4 h-4/5 w-full rounded-2xl bg-[#1E1E1E] border border-[#2D2D2D] shadow-2xl">
+      <div className="mx-4 h-4/5 w-full rounded-2xl border border-[#2D2D2D] bg-[#1E1E1E] shadow-xl shadow-gray-900">
         <div className="h-full p-12">
           <SearchBar onSearchClick={onSearchClick} setSearchAlbumInput={setSearchAlbumInput} />
           <SearchResult albums={albums} />
