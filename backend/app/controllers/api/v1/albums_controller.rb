ead5c5eafe1daef7c344dfe93a3f606d7950272b
dueityou <dueityou@gmail.com>
2025-06-15 17:38:12 +0900

    [add] アルバムの検索結果のデータの取得 #2

--- backend/app/controllers/api/v1/albums_controller.rb
+++ backend/app/controllers/api/v1/albums_controller.rb
@@ -9,6 +9,11 @@ class Api::V1::AlbumsController < ApplicationController
               })
     access_token = JSON.parse(response.body.to_s)["access_token"]
 
-    render json: { response: access_token }
+    # アルバムの検索結果の取得
+    search_words = URI.encode_www_form_component("椎名林檎")
+    response = HTTP.headers("Authorization" => "Bearer #{access_token}")
+        .get("https://api.spotify.com/v1/search?q=#{search_words}&type=album&market=JP")
+    data = JSON.parse(response.body.to_s)
+    render json: { response: data }
   end
 end
