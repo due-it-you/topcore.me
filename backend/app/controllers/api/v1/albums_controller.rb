4da4dce9797f9d8cc6b8d7e8694078b2e1934256
dueityou <dueityou@gmail.com>
2025-06-15 21:13:00 +0900

    [add] ユーザーが入力したアルバム名に応じたアルバム検索機能の実装 #2

--- backend/app/controllers/api/v1/albums_controller.rb
+++ backend/app/controllers/api/v1/albums_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::AlbumsController < ApplicationController
     access_token = JSON.parse(response.body.to_s)["access_token"]
 
     # アルバムの検索結果の取得
-    search_words = URI.encode_www_form_component("椎名林檎")
+    search_words = URI.encode_www_form_component(params[:name])
     response = HTTP.headers("Authorization" => "Bearer #{access_token}")
         .get("https://api.spotify.com/v1/search?q=#{search_words}&type=album&market=JP&limit=30")
     albums_data = JSON.parse(response.body.to_s)["albums"]["items"]
