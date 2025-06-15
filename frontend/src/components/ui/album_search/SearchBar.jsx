81bbc64e41391a5ad69ea819291edef95d1dcbc0
dueityou <dueityou@gmail.com>
2025-06-16 02:38:04 +0900

    [change] 検索フィールドの縦幅を大きくする変更

--- frontend/src/components/ui/album_search/SearchBar.jsx
+++ frontend/src/components/ui/album_search/SearchBar.jsx
@@ -5,7 +5,7 @@ export default function SearchBar({ onSearchClick, setSearchAlbumInput }) {
   return (
     <div className="mb-4 flex items-center justify-center">
       <Input
-        className="w-3/4 rounded-md border border-gray-700 bg-white/5 px-4 py-2 text-white placeholder-gray-400 backdrop-blur focus:ring-2 focus:ring-cyan-500"
+        className="w-3/4 rounded-md border border-gray-300 bg-white/5 px-4 py-5 text-white placeholder-gray-400 backdrop-blur focus:ring-2 focus:ring-cyan-500"
         placeholder="検索したいアルバムを入力してください"
         onChange={(e) => setSearchAlbumInput(e.target.value)}
       />
@@ -13,7 +13,7 @@ export default function SearchBar({ onSearchClick, setSearchAlbumInput }) {
         onClick={onSearchClick}
         variant="secondary"
         size="icon"
-        className="mx-2 size-10 border border-gray-200 bg-[#252A30] hover:bg-[#1E2328] active:bg-[#1CB588]"
+        className="mx-2 size-10 border border-gray-200 bg-[#252A30] hover:bg-[#1E2328] active:bg-cyan-500"
       >
         🔍
       </Button>
