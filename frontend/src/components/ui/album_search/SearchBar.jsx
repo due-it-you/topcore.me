2ebb25c290d50c6b8a6b3c82734ce65a7255ad83
dueityou <dueityou@gmail.com>
2025-06-15 20:15:18 +0900

    [add] アルバム検索ボタンをクリックするとアルバムの画像が表示される #2

--- frontend/src/components/ui/album_search/SearchBar.jsx
+++ frontend/src/components/ui/album_search/SearchBar.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import axios from '../../../../api/lib/apiClient';
 
 export default function SearchBar() {
+  const [albums, setAlbums] = useState(null);
+
+  async function onSearchClick() {
+    const res = await axios.get('/albums/search');
+    const albums = res.data.searchedAlbums;
+    setAlbums(albums);
+  }
   return (
     <div className="mb-4 flex items-center justify-center">
       <Input
@@ -9,12 +18,24 @@ export default function SearchBar() {
         placeholder="検索したいアルバムを入力してください"
       />
       <Button
+        onClick={onSearchClick}
         variant="secondary"
         size="icon"
         className="mx-2 size-10 border border-gray-200 bg-[#252A30] hover:bg-[#1E2328]"
       >
         🔍
       </Button>
+      <div>
+        {albums
+          ? albums.map((album) => {
+              return (
+                <div className="aspect-square rounded-xl" key={album.spotifyId}>
+                  <img src={album.imageUrl} alt={album.name} />
+                </div>
+              );
+            })
+          : ''}
+      </div>
     </div>
   );
 }
