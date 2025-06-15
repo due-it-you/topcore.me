e14147824317cb9906a030445bd025164db3b905
dueityou <dueityou@gmail.com>
2025-06-15 17:28:12 +0900

    [add] アクセストークンの取得処理の追加 #2

--- backend/app/controllers/api/v1/albums_controller.rb
+++ backend/app/controllers/api/v1/albums_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::AlbumsController < ApplicationController
   def search
-    
+    # アクセストークンの取得
+    response = HTTP.headers("Content-Type" => "application/x-www-form-urlencoded")
+              .post("https://accounts.spotify.com/api/token", form: {
+                grant_type: "client_credentials",
+                client_id: ENV['SPOTIFY_CLIENT_ID'],
+                client_secret: ENV['SPOTIFY_CLIENT_SECRET']
+              })
+    access_token = JSON.parse(response.body.to_s)["access_token"]
+
+    render json: { response: access_token }
   end
 end
