1974d5e6254ff9d7a32f36945225e5eebcaecd59
dueityou <dueityou@gmail.com>
2025-06-16 14:46:02 +0900

    [update] 検索結果の各アルバムをDraggableに更新 #4

--- frontend/src/components/ui/album_search/SearchResult.jsx
+++ frontend/src/components/ui/album_search/SearchResult.jsx
@@ -5,10 +5,10 @@ export default function SearchResult({ albums }) {
     <div className="h-7/8 overflow-y-scroll">
       <div className="grid grid-cols-3 gap-1">
         {albums
-          ? albums.map((album) => {
+          ? albums.map((album, index) => {
               return (
                 <div key={album.spotifyId}>
-                  <SearchedAlbum src={album.imageUrl} alt={album.name} />
+                  <SearchedAlbum src={album.imageUrl} alt={album.name} index={index} />
                 </div>
               );
             })
