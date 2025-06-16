54f8d9cf3c5686ce6055fbf313247b369297a968
dueityou <dueityou@gmail.com>
2025-06-16 13:04:07 +0900

    [update] グリッドのマスの初期状態では画像が割り当てられていない状態に更新しました #4

--- frontend/src/components/ui/album_grid_editor/Album.jsx
+++ frontend/src/components/ui/album_grid_editor/Album.jsx
@@ -1,11 +1,13 @@
 export default function Album() {
   return (
     <div className="aspect-square rounded-xl bg-[#0E1012]">
-      <img
-        src="https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
-        alt="set_album"
-        className="rounded-sm"
-      />
+      <div className="">
+        {/* <img
+          src="https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228"
+          alt="set_album"
+          className="rounded-sm"
+        /> */}
+      </div>
     </div>
   );
 }
--- frontend/src/components/ui/album_grid_editor/DropAlbumGrid.jsx
+++ frontend/src/components/ui/album_grid_editor/DropAlbumGrid.jsx
@@ -2,16 +2,18 @@ import Album from './Album';
 
 export default function DropAlbumGrid() {
   return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-2">
-      <Album />
-      <Album />
-      <Album />
-      <Album />
-      <Album />
-      <Album />
-      <Album />
-      <Album />
-      <Album />
+    <div className="aspect-square w-100">
+      <div className="grid grid-cols-3 grid-rows-3 gap-2">
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+        <Album />
+      </div>
     </div>
   );
 }
