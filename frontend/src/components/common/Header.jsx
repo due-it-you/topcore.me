17dc71f3ee3e5babaeafe979ac8f834612a04ec0
dueityou <dueityou@gmail.com>
2025-06-18 01:16:24 +0900

    [update] ヘッダーとアルバム検索カードの影を少し抑えめに

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <div className="h-16 w-full border-b border-gray-800 bg-[#131313] shadow-2xl shadow-gray-900">
+      <div className="h-16 w-full border-b border-gray-800 bg-[#131313] shadow-xl">
         <div className="px-4">
           <img src="/top_core_logo_trimmed.png" alt="top_core_me_logo" className="h-16" />
         </div>
--- frontend/src/components/ui/album_search/AlbumSearchCard.jsx
+++ frontend/src/components/ui/album_search/AlbumSearchCard.jsx
@@ -12,7 +12,7 @@ export default function AlbumSearchCard({
 }) {
   return (
     <div className="flex h-screen w-2/5 items-center">
-      <div className="mx-4 h-4/5 w-full rounded-2xl border border-[#2D2D2D] bg-[#1E1E1E] shadow-xl shadow-gray-900">
+      <div className="mx-4 h-4/5 w-full rounded-xl border border-[#2D2D2D] bg-[#1E1E1E] shadow-xl shadow-gray-900">
         <div className="h-full p-12">
           <SearchBar onSearchClick={onSearchClick} setSearchAlbumInput={setSearchAlbumInput} />
           <SearchResult
