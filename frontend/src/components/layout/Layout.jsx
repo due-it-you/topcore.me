73efcd2c8d0079d6e7a78c8a69ce62ae10a002c6
dueityou <dueityou@gmail.com>
2025-06-18 00:55:54 +0900

    [update] 画面の背景色のstateを追加し、背景色を状態管理するように更新 #8

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 export default function Layout({ children }) {
+  const [color, setColor] = useState('#121212')
   return (
     <>
-      <div className="min-h-screen bg-[#121212]">{children}</div>
+      <div className={`min-h-screen bg-[${color}]`}>{children}</div>
     </>
   );
 }
