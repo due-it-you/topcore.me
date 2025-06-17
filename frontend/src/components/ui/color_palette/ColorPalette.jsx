ef1c7d2a50bb41f6419a5b33af6901a8a3036cae
dueityou <dueityou@gmail.com>
2025-06-18 01:18:49 +0900

    [update] カラーパレットのUIデザインを調整 #8

--- frontend/src/components/ui/color_palette/ColorPalette.jsx
+++ frontend/src/components/ui/color_palette/ColorPalette.jsx
@@ -2,8 +2,8 @@ import Color from './Color';
 
 export default function ColorPalette({ setColor }) {
   return (
-    <div className="mt-8 flex h-2/3 w-full justify-center rounded-2xl bg-[#151A1E]">
-      <div className="grid grid-cols-1 gap-1">
+    <div className="mt-8 flex h-2/3 w-4/5 justify-center rounded-2xl bg-[#151A1E]">
+      <div className="my-4 grid grid-cols-1 gap-1">
         <Color color={'#121212'} className={'border border-gray-600'} setColor={setColor} />
         <Color color={'#3E7CB6'} setColor={setColor} />
         <Color color={'#72A7D3'} setColor={setColor} />
