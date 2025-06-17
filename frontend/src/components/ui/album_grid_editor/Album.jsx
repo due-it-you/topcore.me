f751a5cb4e63ad922c651d4ae25a85a93eb383a7
dueityou <dueityou@gmail.com>
2025-06-17 14:27:32 +0900

    [update] AlbumコンポーネントをDroppableからSortableなコンポーネントに更新 #5

--- frontend/src/components/ui/album_grid_editor/Album.jsx
+++ frontend/src/components/ui/album_grid_editor/Album.jsx
@@ -1,18 +1,20 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { useDroppable } from '@dnd-kit/core';
 import AlbumImage from '../album_search/AlbumImage';
 
 export default function Album({ id, src, alt }) {
-  const { setNodeRef } = useDroppable({
-    id: id,
-  });
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   return (
-    <div ref={setNodeRef} className="aspect-square rounded-xl border border-white bg-[#0E1012]">
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="aspect-square rounded-xl border border-white bg-[#0E1012]">
       <div>
-        <AlbumImage
-          src={src}
-          alt={alt}
-        />
+        <AlbumImage src={src} alt={alt} />
       </div>
     </div>
   );
