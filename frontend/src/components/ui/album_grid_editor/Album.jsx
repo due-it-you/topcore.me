57a106625eb7b8fabbd0749da19b8e09b3846c54
dueityou <dueityou@gmail.com>
2025-06-17 15:04:00 +0900

    [update] グリッドのマスがドラッグされている状態の時、元のマスにあるアルバムの画像が少しだけ透過状態になるように更新 #5

--- frontend/src/components/ui/album_grid_editor/Album.jsx
+++ frontend/src/components/ui/album_grid_editor/Album.jsx
@@ -3,15 +3,24 @@ import { CSS } from '@dnd-kit/utilities';
 import AlbumImage from '../album_search/AlbumImage';
 
 export default function Album({ id, src, alt }) {
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id,
+  });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    opacity: isDragging ? 0.3 : 1,
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="aspect-square rounded-xl border border-white bg-[#0E1012]">
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="aspect-square rounded-xl border border-white bg-[#0E1012]"
+    >
       <div>
         <AlbumImage src={src} alt={alt} />
       </div>
