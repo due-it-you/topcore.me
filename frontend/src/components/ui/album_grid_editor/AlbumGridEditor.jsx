46c706ed5938a63ffcacfd816adb25a43a7f2279
dueityou <dueityou@gmail.com>
2025-06-18 16:30:59 +0900

    [add] リンクを生成ボタン、クリック時のモーダルの表示 #11

--- frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
+++ frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
@@ -1,5 +1,15 @@
 import ColorPalette from '../color_palette/ColorPalette';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import DropAlbumGrid from './DropAlbumGrid';
 
 export default function AlbumGridEditor({ assignedAlbums, color, setColor }) {
@@ -9,9 +19,33 @@ export default function AlbumGridEditor({ assignedAlbums, color, setColor }) {
         <div className="h-full p-12">
           <DropAlbumGrid assignedAlbums={assignedAlbums} />
           <div className="mt-4 text-right">
-            <Button className="bg-[#20C997] font-semibold text-white hover:bg-[#1DB954]">
-              リンクを生成
-            </Button>
+            <Dialog>
+              <form>
+                <DialogTrigger asChild>
+                  <Button className="bg-[#20C997] font-semibold text-white hover:bg-[#1DB954]">
+                    リンクを生成
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="border border-[#646464] bg-[#151A1E] p-12 text-white sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>ユーザー名とアイコンの設定</DialogTitle>
+                    <DialogDescription>
+                      ※アイコンの設定は任意です
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid gap-3"></div>
+                    <div className="grid gap-3"></div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit">Save changes</Button>
+                  </DialogFooter>
+                </DialogContent>
+              </form>
+            </Dialog>
           </div>
         </div>
         <div className="h-full w-1/4">
