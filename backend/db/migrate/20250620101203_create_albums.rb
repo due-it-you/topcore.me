c9bbfc9b208deb14b5fa123c4d05bda845a19d99
dueityou <dueityou@gmail.com>
2025-06-20 19:20:59 +0900

    [add] albumsテーブルの新規作成 #12

--- backend/db/migrate/20250620101203_create_albums.rb
+++ backend/db/migrate/20250620101203_create_albums.rb
@@ -1,6 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[7.2]
   def change
     create_table :albums do |t|
+      t.string :spotify_id, null: false
       t.timestamps
     end
   end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_20_080218) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_20_101203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_20_080218) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "albums", force: :cascade do |t|
+    t.string "spotify_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profile_cards", force: :cascade do |t|
