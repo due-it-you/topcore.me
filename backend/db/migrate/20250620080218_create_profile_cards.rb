d1f705162b0a426134b9b44a8770db5fd2a21699
dueityou <dueityou@gmail.com>
2025-06-20 17:32:22 +0900

    [add] profile_cardsテーブルのマイグレーションファイルの新規作成 #12

--- backend/db/migrate/20250620080218_create_profile_cards.rb
+++ backend/db/migrate/20250620080218_create_profile_cards.rb
@@ -1,6 +1,11 @@
 class CreateProfileCards < ActiveRecord::Migration[7.2]
   def change
     create_table :profile_cards do |t|
+      t.string :slug, null: false
+      t.string :bg_color, null: false
+      t.string :display_name, null: false
+      t.integer :grid_rows, null: false
+      t.integer :grid_columns, null: false
       t.timestamps
     end
   end
