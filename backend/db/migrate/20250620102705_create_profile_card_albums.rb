ee4760b4152f2818b6cddec5f2f7af64ef9b55b9
dueityou <dueityou@gmail.com>
2025-06-20 19:30:07 +0900

    [add] profile_card_albumsテーブルのマイグレーションファイルの更新 #12

--- backend/db/migrate/20250620102705_create_profile_card_albums.rb
+++ backend/db/migrate/20250620102705_create_profile_card_albums.rb
@@ -1,7 +1,7 @@
 class CreateProfileCardAlbums < ActiveRecord::Migration[7.2]
   def change
     create_table :profile_card_albums do |t|
-      t.string :position
+      t.string :position, null: false
       t.references :profile_card, null: false, foreign_key: true
       t.references :album, null: false, foreign_key: true
 
