ad00cd6d57e384369545f6048757aa4bc3e1a3b0
dueityou <dueityou@gmail.com>
2025-06-20 17:14:23 +0900

    [add] ProfileCardモデルにバリデーションの追加 #12

--- backend/app/models/profile_card.rb
+++ backend/app/models/profile_card.rb
@@ -1,2 +1,7 @@
 class ProfileCard < ApplicationRecord
+  validates :slug, presence: true, length: { in: 1..20 }, uniqueness: true
+  validates :bg_color, presence: true
+  validates :display_name, presence: true, length: { in: 1..20 }
+  validates :grid_rows, presence: true, numericality: { only_integer: true }
+  validates :grid_columns, presence: true, numericality: { only_integer: true }
 end
