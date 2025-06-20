06f709f63aa08d194d15ecf019c02c629e23513b
dueityou <dueityou@gmail.com>
2025-06-20 15:37:36 +0900

    [add] profile_cards_paramsのストロングパラメータの追加 #12

--- backend/app/controllers/api/v1/profile_cards_controller.rb
+++ backend/app/controllers/api/v1/profile_cards_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::ProfileCardsController < ApplicationController
   def create
     params["params"]["albums"].each do |album|
-      album[""]
+      album["spotify_id"]
     end
+  end
+
+  private
+
+  def profile_cards_params
+    params.require(:params).permit(
+      profile_cards: [:display_name, :grid_rows, :grid_columns],
+      albums: [:id, :src, :alt, :spotify_id]
+    )
   end
 end
