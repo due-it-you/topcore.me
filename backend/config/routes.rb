68f3eedb4b404bf9868d758e0873310628af26ea
dueityou <dueityou@gmail.com>
2025-06-20 13:02:14 +0900

    [delete] albums#createのルーティングは今後使わないので削除 #12

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :albums, only: %i[create] do
+      resources :albums, only: %i[] do
         collection do
           get :search
         end
