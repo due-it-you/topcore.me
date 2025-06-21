d73db3ab17b0f60fd19990f9779334e520a45862
dueityou <dueityou@gmail.com>
2025-06-21 17:20:40 +0900

    [fix] Rails APIにリクエスト出来ていない不具合を修正 #60

--- frontend/src/components/ui/profile_cards/ProfileCard.jsx
+++ frontend/src/components/ui/profile_cards/ProfileCard.jsx
@@ -1,14 +1,16 @@
 import { useParams } from 'react-router';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from './../../../../api/lib/apiClient';
 
 export default function ProfileCard() {
   const params = useParams();
   useEffect(() => {
-    if (!params.slug) return;
+    async function fetchData() {
+      const response = await axios.get(`/profile_cards/${params.slug}`);
+    }
 
-    const fetchData = async () => {
-      const res = await axios.get(`profile_cards/${params.slug}`);
-    };
-  });
+    fetchData();
+  }, []);
+
+  return <></>;
 }
