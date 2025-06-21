ecbd4216e36e9c955d98b96e2835d3cd80a7271d
dueityou <dueityou@gmail.com>
2025-06-21 16:17:29 +0900

    [add] slugを用いてRailsのprofile_cards#showへリクエスト #60

--- frontend/src/components/ui/profile_cards/ProfileCard.jsx
+++ frontend/src/components/ui/profile_cards/ProfileCard.jsx
@@ -1,6 +1,14 @@
-import { useParams } from "react-router";
-import { useState, useEffect } from "react";
+import { useParams } from 'react-router';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ProfileCard() {
   const params = useParams();
+  useEffect(() => {
+    if (!params.slug) return;
+
+    const fetchData = async () => {
+      const res = await axios.get(`profile_cards/${params.slug}`);
+    };
+  });
 }
