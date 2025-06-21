08d0ca8bc6fec46f035c95b8bf70b10f258706c6
dueityou <dueityou@gmail.com>
2025-06-21 16:02:05 +0900

    [add] music/:slug のルーティング #60

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App.jsx';
+import ProfileCard from './components/ui/profile_cards/ProfileCard';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="music/:slug" element={<ProfileCard />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
