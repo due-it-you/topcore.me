030f80529884f83927a3d38f992e740dcf4dbafc
dueityou <dueityou@gmail.com>
2025-06-21 13:44:02 +0900

    [add] クリップボードにリンクをコピーできる機能を実装 #13

--- frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
+++ frontend/src/components/ui/album_grid_editor/AlbumGridEditor.jsx
@@ -36,6 +36,17 @@ export default function AlbumGridEditor({
       setDisabledGenerateLinkButton(false);
     }
   }
+  function onCopyClipboardClick() {
+    const txt = document.getElementById('txt');
+    navigator.clipboard.writeText(txt.textContent).then(
+      () => {
+        console.log('コピー完了');
+      },
+      () => {
+        console.log('コピー失敗');
+      },
+    );
+  }
   return (
     <div className="flex h-screen w-3/5 items-center justify-center">
       <div className="mx-4 flex aspect-square w-3/4">
@@ -87,15 +98,20 @@ export default function AlbumGridEditor({
                   {step === 'success' && (
                     <div>
                       <DialogHeader className="mt-4">
-                        <DialogTitle className="text-xl">リンクの生成が完了しました！🎉</DialogTitle>
-                        <DialogDescription className="font-semibold text-md">
+                        <DialogTitle className="text-xl">
+                          リンクの生成が完了しました！🎉
+                        </DialogTitle>
+                        <DialogDescription className="text-md font-semibold">
                           コピーしてSNSのプロフィールに貼りましょう!!
                         </DialogDescription>
                       </DialogHeader>
                       <div className="mt-4 rounded-xl bg-gray-800 text-xl text-white">
                         <div className="flex items-center justify-between p-4">
-                          <div className="">https://topcore.me/music/{slug}</div>
-                          <button className="aspect-square rounded-sm h-8 bg-white text-black hover:bg-gray-200">
+                          <div id="txt">https://topcore.me/music/{slug}</div>
+                          <button
+                            onClick={onCopyClipboardClick}
+                            className="aspect-square h-8 rounded-sm bg-white text-black hover:bg-gray-200"
+                          >
                             <FontAwesomeIcon icon={faClipboard} />
                           </button>
                         </div>
