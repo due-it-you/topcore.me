c0a5d504f6451229cc5f8fe4c0ce9c81474facc0
dueityou <dueityou@gmail.com>
2025-06-21 19:36:09 +0900

    [add] レスポンスのalbums, profileCardの情報をstateで状態保持 #60

--- frontend/src/components/ui/profile_cards/ProfileCard.jsx
+++ frontend/src/components/ui/profile_cards/ProfileCard.jsx
@@ -4,9 +4,13 @@ import axios from './../../../../api/lib/apiClient';
 
 export default function ProfileCard() {
   const params = useParams();
+  const [albums, setAlbums] = useState(null);
+  const [profileCard, setProfileCard] = useState(null);
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get(`/profile_cards/${params.slug}`);
+      setAlbums(response.data.albums);
+      setProfileCard(response.data.profileCard);
     }
 
     fetchData();
