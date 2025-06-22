e3ddae94c5e878d7f8cd1babb7cae39024991c39
dueityou <dueityou@gmail.com>
2025-06-22 14:11:00 +0900

    [fix] ストロングパラメータでavatarを許可していなかったので修正 #58

--- backend/app/controllers/api/v1/profile_cards_controller.rb
+++ backend/app/controllers/api/v1/profile_cards_controller.rb
@@ -2,6 +2,7 @@ require 'securerandom'
 
 class Api::V1::ProfileCardsController < ApplicationController
   def create
+    binding.pry
     profile_card = ProfileCard.create_with_albums!(profile_cards_params)
     render json: { slug: profile_card.slug }
   rescue ActiveRecord::RecordInvalid => e
@@ -52,7 +53,7 @@ class Api::V1::ProfileCardsController < ApplicationController
 
   def profile_cards_params
     params.permit(
-      profile_cards: [:display_name, :bg_color, :grid_rows, :grid_columns],
+      profile_cards: [:display_name, :bg_color, :grid_rows, :grid_columns, :avatar],
       albums: [:id, :src, :alt, :spotify_id]
     )
   end
