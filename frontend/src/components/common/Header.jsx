cd80cffdec691df048e54406b7e5993aaf9663a5
dueityou <dueityou@gmail.com>
2025-06-23 23:57:33 +0900

    [add] ヘッダーのロゴをクリックするとプロフィールカードを作成できるホーム画面へ遷移するリンクを追加

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,11 +1,13 @@
 export default function Header() {
   return (
     <>
-      <div className="h-16 w-full border-b border-gray-800 bg-[#131313] shadow-xl">
-        <div className="px-4">
-          <img src="/top_core_logo_trimmed.png" alt="top_core_me_logo" className="h-16" />
+      <a href="/">
+        <div className="h-16 w-full border-b border-gray-800 bg-[#131313] shadow-xl">
+          <div className="px-4">
+            <img src="/top_core_logo_trimmed.png" alt="top_core_me_logo" className="h-16" />
+          </div>
         </div>
-      </div>
+      </a>
     </>
   );
 }
