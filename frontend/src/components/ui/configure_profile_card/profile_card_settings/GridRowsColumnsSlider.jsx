0c570d8f2e612fb9449f8c914d74573bcb5f1c99
dueityou <dueityou@gmail.com>
2025-06-29 23:47:37 +0900

    [add] グリッドの列を変更するための初期状態のスライダーの表示 #107

--- frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
+++ frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
@@ -1,7 +1,17 @@
+import { Slider } from '../../slider';
+
 export default function GridRowsColumnsSlider() {
   return (
     <div>
-      <div>test</div>
+      <div>
+        <div className="font-bold text-white">グリッドの列</div>
+        <div className="flex mt-4">
+          <div className="mr-2 flex aspect-square w-1/10 items-center justify-center rounded-md bg-gray-600 text-white">
+            <div>3</div>
+          </div>
+          <Slider className="w-9/10" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
