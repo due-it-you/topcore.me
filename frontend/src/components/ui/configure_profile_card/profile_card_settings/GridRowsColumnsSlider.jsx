484f7c1f44310cd4aa9311c610d27d120663810f
dueityou <dueityou@gmail.com>
2025-06-29 23:54:11 +0900

    [update] スライダーのレンジを更新 #107

--- frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
+++ frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
@@ -9,7 +9,7 @@ export default function GridRowsColumnsSlider() {
           <div className="mr-2 flex aspect-square w-1/10 items-center justify-center rounded-md bg-gray-600 text-white">
             <div>3</div>
           </div>
-          <Slider className="w-9/10" />
+          <Slider min={2} max={10} defaultValue={[3]} className="w-9/10" />
         </div>
       </div>
     </div>
