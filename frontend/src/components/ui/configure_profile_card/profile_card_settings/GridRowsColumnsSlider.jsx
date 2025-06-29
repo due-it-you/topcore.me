818f1c9ddb6874fcf30492ec4cea7a56825518cb
dueityou <dueityou@gmail.com>
2025-06-29 23:59:36 +0900

    [add] スライダーのレンジを変更すると表示されているレンジの値が動的に変更される #107

--- frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
+++ frontend/src/components/ui/configure_profile_card/profile_card_settings/GridRowsColumnsSlider.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { Slider } from '../../slider';
 
 export default function GridRowsColumnsSlider() {
+  const [gridRows, setGridRows] = useState(3);
   return (
     <div>
       <div>
         <div className="font-bold text-white">グリッドの列</div>
-        <div className="flex mt-4">
+        <div className="mt-4 flex">
           <div className="mr-2 flex aspect-square w-1/10 items-center justify-center rounded-md bg-gray-600 text-white">
-            <div>3</div>
+            <div>{gridRows}</div>
           </div>
-          <Slider min={2} max={10} defaultValue={[3]} className="w-9/10" />
+          <Slider
+            min={2}
+            max={10}
+            defaultValue={[3]}
+            onValueChange={(e) => setGridRows(e[0])}
+            className="w-9/10"
+          />
         </div>
       </div>
     </div>
